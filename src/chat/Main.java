2a3cba9239b3a529e8b584288b87a044fa00e81a
Rodrigo Mayer <mayerrodrigo98@gmail.com>
2021-07-19 19:48:45 -0300

    Fix Main

--- src/chat/Main.java
+++ src/chat/Main.java
@@ -3,13 +3,6 @@ package chat;
 class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Manés");
-
-
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
-
+        System.out.println("Hello World");
     }
 }
