98c2d9bf1a24c60ce09252280cc6dc8200d55177
Luiz Rodrigo <11786486+luizrcs@users.noreply.github.com>
2021-08-02 18:04:31 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,10 @@
-set ZK="C:\Software\zookeeper-3.4.14"
-set OUT=".\out\production\sdProjetoFinal"
+:: Caminho para o ZooKeeper
+set ZK=""
+
+:: Caminho para os arquivos compilados
+set OUT=""
+
 set CP_ZK=%OUT%;%ZK%\zookeeper-3.4.14.jar;%ZK%\lib\slf4j-log4j12-1.7.25.jar;%ZK%\lib\slf4j-api-1.7.25.jar;%ZK%\lib\log4j-1.2.17.jar
-java -cp %CP_ZK% -Dlog4j.configuration=file:%ZK%\conf\log4j.properties chat.ZooKeeperClient zookeeper.luizrcs.com.br
+
+:: "localhost" define o endereço onde o ZooKeeper está sendo executado
+java -cp %CP_ZK% -Dlog4j.configuration=file:%ZK%\conf\log4j.properties chat.ZooKeeperClient localhost
