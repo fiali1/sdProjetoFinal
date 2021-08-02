761683a52e58a6e8a9f3cf8606bd35b711db8923
Gabriel Fiali <43157894+fiali1@users.noreply.github.com>
2021-08-02 18:09:42 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
-export ZK=/home/gabriel/zookeeper
-export OUT=/home/gabriel/Documentos/UFABC/qs_2021.2/SD/sdProjetoFinal/out/production/sdProjetoFinal/
+# Diretório do ZooKeeper
+export ZK=""
+
+# Caminho para os arquivos compilados
+export OUT=""
+
+# Último parâmetro: local onde o ZooKeeper está hospedado (por padrão, "localhost")
 export CP_ZK=$OUT:$ZK'/zookeeper-3.4.14.jar':$ZK'/lib/slf4j-log4j12-1.7.25.jar':$ZK'/lib/slf4j-api-1.7.25.jar':$ZK'/lib/log4j-1.2.17.jar'
 java -cp $CP_ZK -Dlog4j.configuration=file:$ZK/conf/log4j.properties chat.ZooKeeperClient localhost
